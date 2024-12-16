87644430607ee2dcb47a7fd36c2357111fd2fb33
Priyanshi Kirar <patelpriyanshi866@gmail.com>
2024-12-16 18:09:29 +0530

    16 dec

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const express=require("express");
-const app=express();
-app.get("/",function(req,res){
-    res.send('app ');
+const express = require("express");
+const app = express();
+const axios = require('axios');
+const Redis=require("ioredis");
+app.get("/", function (req, res) {
+    res.send('app is working ');
 })
-app.listen(3000,function(){
-    console.log("server is woring on 3000");
+
+
+app.get("/create-ride", async (req, res) => {
+    try {
+        console.log("Requesting distance from map server...");
+        const result = await axios.get("http://localhost:3001/distance");
+        console.log("Received response:", result.data);
+        res.send(result.data);
+    } catch (error) {
+        console.error("Error fetching distance:", error.message);
+        res.status(500).send("Failed to fetch of distance");
+    }
 });
+
+
+app.listen(3000, () => {
+    console.log("Server is working on port 3000");
+})
